6a370005413f427ef0f3dafbe8936ccd3d214154
Bezibun <fauzanputra@gmail.com>
2025-03-11 11:41:58 +0700

    nama

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Project 1",
-  description: "Project pertama menggunakan ",
+  title: "Portofolio",
+  description: "Portofolio Fauzan ",
 };
 
 export default function RootLayout({ children }) {
